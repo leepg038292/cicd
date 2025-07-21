9707ad38bde4751f0e6983eae7be850a3ab416fa
leepg038292 <leepg038292@naver.com>
2025-07-21 19:56:45 +0900

    스웨거 테스트2

--- src/main/java/example/demo/ctr/helloctr.java
+++ src/main/java/example/demo/ctr/helloctr.java
@@ -4,10 +4,13 @@ package example.demo.ctr;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
+@RequestMapping("/hello")
 public class helloctr {
 
 
